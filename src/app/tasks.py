62eb6d84c23ac95e8e07362dcc6c1ea2aec0ea09
Maleh Duenas <maledr5@hotmail.es>
2018-04-29 10:06:04 -0500

    adds site with links to spreadsheets

--- src/app/tasks.py
+++ src/app/tasks.py
@@ -2,14 +2,18 @@ from invoke import task
 
 @task
 def push(ctx):
-	ctx.run("git push && git push heroku")
+	ctx.run("git push && git push heroku master")
+
+@task
+def pull(ctx):
+	ctx.run("git pull && git pull heroku master")
 
 @task
 def run(ctx):
 	ctx.run("python app.py")
 
 @task
-def setPath():
+def setPath(ctx):
 	import os, inspect, sys
 	cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
 	if cmd_folder not in sys.path:
